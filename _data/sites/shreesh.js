69361fc00244314d687964d712eaa20b2b8123b4
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2020-07-13 10:42:56 -0400

    Update site data

---
+++ _data/sites/shreesh.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "shreesh.dev", // optional, falls back to object key
+	description: "Personal web site",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.shreesh.dev/",
+		"https://www.shreesh.dev/about/",
+		"https://www.shreesh.dev/posts/"
+	]
+};
--- _data/sites/ssg.js
+++ _data/sites/ssg.js
@@ -8,17 +8,7 @@ module.exports = {
 		freshChrome: "run"
 	},
 	urls: [
-		"https://www.11ty.dev/",
-		"https://www.gatsbyjs.org/",
-		"https://gohugo.io/",
-		"https://nextjs.org/",
-		"https://nuxtjs.org/",
-		"https://gridsome.org/",
-		"https://vuepress.vuejs.org/",
-		"https://v2.docusaurus.io/",
-		"https://docsify.js.org/",
-		"https://jekyllrb.com/",
-		"https://hexo.io/",
-		"https://sapper.svelte.dev/",
+		"https://stenciljs.com/",
+		"https://www.strikeoff.app/"
 	]
 };
